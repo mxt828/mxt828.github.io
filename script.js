c49a0563c8fc6d43208f0532595097d543288a4a
Year UP <yearup@Year-UPs-Mac-mini.local>
2014-05-22 18:33:17 -0700

    Canvas assignment

---
+++ script.js
@@ -0,0 +1,8 @@
+var canvas = document.querySelector('canvas');
+var ctx    = canvas.getContext('2d');
+ctx.fillStyle = '#3399FF';
+ctx.fillRect(0, 0, 500, 500);
+ctx.fillStyle = '#000000';
+ctx.fillRect(80, 50, 80, 80);
+ctx.fillRect(300, 50, 80, 80);
+ctx.fillRect(55, 300, 400, 40);
